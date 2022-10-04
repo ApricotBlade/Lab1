cd31a97daf298bf4fe21dd5631fc11238c51adb7
ApricotBlade <nvk23vek@gmail.com>
2022-10-05 01:30:29 +0400

    Добавление функции сохранения изображения. Проверка на ошибки при попытке скачивания, сохренение изображения в нужную папку-архив

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import requests
 from bs4 import BeautifulSoup
 
@@ -29,8 +30,23 @@ def getlink(url, pos):
         link = link.split('"origin":{"')
         link = link[1].split('"url":"')
         link = link[1].split('"}')
-        if link[0].find('.jpg.') != -1:
+        if link[0].find(".jpg") != -1:
             return link[0]
     return False
 
+
+def saveimg(link, serialnum, imgclass):
+    try:
+        response = requests.get(link, timeout=1, headers={"User-Agent": "Mozilla/5.0"})
+        if response:
+            os.chdir(f'{imgclass}')
+            name = f'{serialnum:04}.jpg'
+            open(name, 'wb').write(response.content)
+            os.chdir('..')
+            return True
+        return False
+    except requests.exceptions.Timeout:
+        return False
+    except requests.exceptions.ConnectionError:
+        return False
 
